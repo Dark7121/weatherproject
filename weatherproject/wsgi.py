d4b21eb2b133b7519a8e187423efb6fe54315b62
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-08-13 23:38:09 +0530

    Update wsgi.py

--- weatherproject/wsgi.py
+++ weatherproject/wsgi.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weatherapplication.settings")
 application = get_wsgi_application()
 app = application
