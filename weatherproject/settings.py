d693c99f51fe82175c7fac7069c4ab41ec99fa66
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-08-13 22:05:29 +0530

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
 from environs import Env
 import dj_database_url
