121e4f5d84e68c0d1da06f55a5189a5460845850
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-08-14 00:59:55 +0530

    Update urls.py

--- weatherapplication/urls.py
+++ weatherapplication/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    #path('', views.home, name='home'),
-    path('weather/', views.home, name='weather'),
+    path('', views.home, name='home'),
 ]
