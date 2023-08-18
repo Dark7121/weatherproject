7ac7fb0ace4750413fa17be682c1ca5b61f9017e
DarkMaster7121 <67087425+Dark7121@users.noreply.github.com>
2023-08-19 03:29:23 +0530

    Update urls.py

--- weatherapplication/urls.py
+++ weatherapplication/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('current', views.current_weather, name='current weather'),
+    path('forecast', views.weather_forecast, name='forecast weather'),
+    path('historical', views.historical_weather, name='historical weather'),
+    path('sign-up', views.signup, name='signup'),
+    path('log-in', views.login, name='login'),
 ]
